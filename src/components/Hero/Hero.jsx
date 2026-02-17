3033a5eb223ec8aaa8ee0038462aac0dfd41f2ba
ashuchauhan5120-hub <ashuchauhan5120@gmail.com>
2026-02-17 08:48:27 +0530

    reponsiveness fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./Hero.css"
 
 const Hero = () => {
@@ -9,7 +10,9 @@ const Hero = () => {
             <div>
                 <h1 id='res-name'>SAVORY</h1>
                 <p className='res-intro'>Authentic Italian Cuisine in the Heart of the City</p>
-                <button className='menu-btn'>View Menu</button>
+                 <Link to="/menu">
+                    <button className='menu-btn'>View Menu</button>
+                </Link>
             </div>
         </section>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className='wrapper'>
       <div className='resturent-name'>
         SAVORY
       </div>
-      <ul className='nav-links'>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li id='order-btn'><Link to="/Order Now">Order Now</Link></li>
+
+      {/* Custom Hamburger Bars */}
+      <div 
+        className={`hamburger-bars ${isOpen ? 'active' : ''}`} 
+        onClick={toggleMenu}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+        <li><Link to="/menu" onClick={closeMenu}>Menu</Link></li>
+        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
+        <li id='order-btn'><Link to="/order">Order Now</Link></li>
       </ul>
     </nav>
   )
